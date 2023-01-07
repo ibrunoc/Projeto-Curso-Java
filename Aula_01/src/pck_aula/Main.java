8c77aa97fe47da5d260e7a3e7d147bc90aca46f9
Bruno Costa <ihyperbr@gmail.com>
2023-01-07 00:03:50 -0300

    Update Main.java

--- Aula_01/src/pck_aula/Main.java
+++ Aula_01/src/pck_aula/Main.java
@@ -2,7 +2,6 @@ package pck_aula;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
