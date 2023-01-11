a850db88e1323c81e82856c51da36ea46c8086ba
Bruno Costa <ihyperbr@gmail.com>
2023-01-11 13:09:20 -0300

    Update Consulta.java

--- ConsultaMedica/src/pkg_testeclasse/Consulta.java
+++ ConsultaMedica/src/pkg_testeclasse/Consulta.java
@@ -6,7 +6,6 @@ public class Consulta{
     public int id_medico;
 
     public void VerConsulta() {
-
         System.out.println("A idenficação do Paciente: " + this.id_paciente + "\n" + "A idenficação do Medico: " + this.id_medico);
     }
 }
