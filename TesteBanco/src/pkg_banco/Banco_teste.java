44907a5c669022ed5e65b133939d3b8dee70596c
Bruno Costa <ihyperbr@gmail.com>
2023-01-18 10:16:39 -0300

    Update Banco_teste.java

--- TesteBanco/src/pkg_banco/Banco_teste.java
+++ TesteBanco/src/pkg_banco/Banco_teste.java
@@ -1,8 +1,7 @@
 package pkg_banco;
 
 public class Banco_teste {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {        
         ContaCorrente Status = new ContaCorrente("Bruno", 2000.00);
 
         Status.VerSaldo();
