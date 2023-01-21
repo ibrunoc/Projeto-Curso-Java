85bee7b96fe985871804124086b666d2440edf21
Bruno Costa <ihyperbr@gmail.com>
2023-01-21 20:52:26 -0300

    Update While.java

--- Aula_01/src/pck_aula/While.java
+++ Aula_01/src/pck_aula/While.java
@@ -3,16 +3,13 @@ import org.w3c.dom.ls.LSOutput;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-
 public class While {
     public static void main(String[] args) {
-
         int n = 1;
 
         while (n<=10){
             System.out.println(n + "x");
             n+=2;
         }
-
     }
 }
