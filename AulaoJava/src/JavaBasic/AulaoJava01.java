aeea62fd60025583ee165b515ef663761c3ce33d
Bruno Costa <ihyperbr@gmail.com>
2023-01-21 20:51:30 -0300

    Update AulaoJava01.java

--- AulaoJava/src/JavaBasic/AulaoJava01.java
+++ AulaoJava/src/JavaBasic/AulaoJava01.java
@@ -1,9 +1,7 @@
 package JavaBasic;
 
-public class AulaoJava01 {
-    
+public class AulaoJava01 {    
     public static void main(String[] args) {
         System.out.println("Bruno");
-    }
-    
+    }    
 }
